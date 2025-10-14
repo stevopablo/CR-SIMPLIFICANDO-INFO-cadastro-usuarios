aa773a01bc4d1fcb9562b39d7e6f15f1b25c2003
pablo <stevaopabllo@gmail.com>
2025-10-14 09:38:40 -0300

    feat <server>: configuracao necessaria

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const app = express();
 const cors = require('cors')
 require('dotenv').config()
+const app = express();
+const userRoutes = require('./src/Routes/UserRoutes'); 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.use(cors({
     origin:'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
+
+app.use('/api/user', userRoutes);
 
 app.get("/", (req, res) => {
     res.status(200).send(`<h1>CR SIMPLIFICANDO INFO LTDA</h1>`);
