b5c3c0a180e15419cbc19162fed884f8710390ab
pablo <stevaopabllo@gmail.com>
2025-10-14 09:32:44 -0300

    fix <user route> erro no endpoint update

--- src/Routes/UserModel.js
+++ src/Routes/UserModel.js
@@ -6,7 +6,7 @@ router.get('/', UserController.getAllUsersHandler)
 router.get('/:id', UserController.getUserByIdHandler)
 router.post('/', UserController.createUserHandler)
 router.post('/login', UserController.loginUserHandler)
-router.put('/', UserController.updateUserHandler)
+router.put('/:id', UserController.updateUserHandler)
 router.delete('/:id', UserController.deleteUserHandler)
 
 module.exports = router
